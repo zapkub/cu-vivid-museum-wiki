28f8c60fff82e3e266ff90457585f54f86fd8f19
Rungsikorn Rungsikavarnich <rungsikorn@me.com>
2017-01-06 01:40:32 +0700

    Init stack

---
+++ src/cms/models/Garden.js
@@ -0,0 +1,35 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Gallery Model
+ * =============
+ */
+
+var Gallery = new keystone.List('Garden', {
+	autokey: { from: 'name', path: 'key', unique: true },
+});
+
+Gallery.add({
+	name: { type: String, required: true },
+	publishedDate: { type: Date, default: Date.now },
+	heroImage: { type: Types.CloudinaryImage },
+	images: { type: Types.CloudinaryImages },
+	cuid: { type: String },
+	blockNo: { type: Number },
+	slotNo: { type: String },
+	scientificName: { type: String },
+	localName: { type: String },
+	otherName: { type: String },
+	duplicateAmount: { type: Number, default: 0 },
+	family: { type: String },
+	collector_en: { type: String },
+	collector_th: { type: String },
+	locationName: { type: String },
+	habit: { type: String },
+	altitude: { type: String },
+	date: { type: Date },
+	note: { type: String },
+});
+
+Gallery.register();
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+// @flow
+import React from 'react';
+
+const g: string = 'b';
+export default (props: string) => <div>Hello world!!</div>;
