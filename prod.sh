1ecd19e9a9706263fcc58328cf4a08ed9f0a0340
Rungsikorn Rungsikavarnich <rungsikorn@me.com>
2017-01-14 20:50:36 +0700

    Finalize Production deployment script

--- prod.sh
+++ prod.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
-# docker build -t museum .
+
+docker-compose stop
+docker-compose rm -f
+
+docker build -t museum --rm .
 cp ./nginx/nginx.conf.example ./nginx/nginx.conf
+
 docker-compose -f docker-compose.yml up -d --no-recreate
