119b1f74eca0a294eaa9e90fdd59c37ff618eb15
Rungsikorn Rungsikavarnich <rungsikorn@me.com>
2017-01-22 10:50:17 +0700

    Edit prod sh

--- prod.sh
+++ prod.sh
@@ -6,4 +6,4 @@ docker-compose rm -f
 docker build -t museum --rm .
 cp ./nginx/nginx.conf.example ./nginx/nginx.conf
 
-docker-compose -f docker-compose.yml up -d --no-recreate
+docker-compose -f docker-compose.yml up -d
