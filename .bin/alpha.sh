41ded9621061e9acb89e62d4e44f3224496e62d9
Rungsikorn Rungsikavanich <rungsikorn@me.com>
2017-03-14 01:09:44 +0700

    disalbe sudo scrypt

--- .bin/alpha.sh
+++ .bin/alpha.sh
@@ -1,5 +1,5 @@
 docker-compose stop
 docker-compose up -d --no-recreate
-sudo npm run build
-sudo npm run down
-sudo NODE_ENV=production npm run up
+npm run build
+npm run down
+NODE_ENV=production npm run up
