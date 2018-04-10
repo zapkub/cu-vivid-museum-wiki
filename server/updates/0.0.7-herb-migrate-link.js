94ef86105abc15a5c3e877c6ab6b25184a1f5bb0
Rungsikorn Rungsikavarnich <rungsikorn@icloud.com>
2018-04-11 02:12:21 +0700

    update link migrate

---
+++ server/updates/0.0.7-herb-migrate-link.js
@@ -0,0 +1,20 @@
+
+const keystone = require('keystone')
+module.exports = async (done) => {
+  const Herbarium = keystone.list('Herbarium')
+  const Garden = keystone.list('Garden')
+  const Museum = keystone.list('Museum')
+
+  const h = await Herbarium.model.find({})
+  for (let i of h) {
+    i.images = i.images.map(image => {
+      return {
+        ...image,
+        url: image.url.replace('https://vimu.blob.core.windows.net/vimu-storage', '')
+      }
+    })
+    await i.save()
+    console.log(i.images)
+  }
+  done()
+}
